9a6d603d5aad1d7efc22df43c9d20f27e702e7ae
Luiz <luiz22gcs@gmail.com>
2025-11-24 20:49:55 -0300

    Separação de login

---
+++ services/auth_service.py
@@ -0,0 +1,17 @@
+from models.user import UserModel
+
+
+class AuthService:
+    def __init__(self):
+        self.user_model = UserModel()
+
+    def login(self, email, senha):
+        todos_usuarios= self.user_model.get_all()
+
+        for usuario in todos_usuarios:
+            if usuario.email == email:
+                if usuario.verificar_senha(senha):
+                    return usuario
+        return None
+auth_service = AuthService()
+
