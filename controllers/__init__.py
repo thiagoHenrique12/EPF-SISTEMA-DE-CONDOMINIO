b098bab14ac9c980a5029ce69c28cf724499ff24
Thiago Henrique <estudos.trabalhos12@gmail.com>
2025-11-30 19:18:30 -0300

    metodos adicionar e editar usuario

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -26,8 +26,10 @@ def init_controllers(app: Bottle):
     app.route('/users',                method='GET',  callback=user_controller.list_users)
     #user_form não foi implementado ainda
     # app.route('/users/new',            method='GET',  callback=user_controller.new_user_form)
-    app.route('/users',                method='POST', callback=user_controller.add_user)
-    app.route('/users/delete/<user_id>', method='POST', callback=user_controller.delete_user)
+    app.route('/users/add', method=['GET', 'POST'], callback=user_controller.add_user)   
+  
+    app.route('/users/edit/<user_id>', method=['GET', 'POST'], callback=user_controller.edit_user)
+
     print('✅ Todas as rotas registradas com sucesso!')
 
 
